99a0acb826c5a9b609f5bce709f794e936dca5dd
Angel Mamberto <107218323+AngelMamberto@users.noreply.github.com>
2024-04-22 09:29:56 -0300

    Test with JSON and Readme

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 # Config and imports
 import helpers
+import requests
+import pandas
+import io
+import json
 
 config = helpers.readConfig("config.ini")
 
@@ -9,6 +13,29 @@ config = helpers.readConfig("config.ini")
 
 url = helpers.createBasicAPIUrl(config)
 
+response = requests.get(url).content
+# print(response)
 
+# print(response.json())
+# a = pandas.read_csv(io.StringIO(response.decode('utf-8')))
+
+# print(response.json())
+
+po = json.loads(response.decode('utf-8'))
+location = po["location"]
+forecasts = po["forecast"]
+
+# print(location)
+# print(forecasts)
+
+# f = open("lala.json", "x")
+# f.write(response.decode('utf-8'))
+# a = pandas.read_json("lala.json")
+# print(a)
+
+f = open("files/location.json", "x")
+f2 = open("files/forecasts.json", "x")
+f.write(json.dumps(location))
+f2.write(json.dumps(forecasts))
 
 # Meter datos en Redshift
